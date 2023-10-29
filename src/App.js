4e3822f330ddf2748318aaa8e03e00deb98861d6
halitsancaktar34 <halitsancaktar34@gmail.com>
2023-10-30 00:24:40 +0300

    Project Completed

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Pokegame from "./Pokegame";
+import "./Pokecard.css"
+
+function App() {
+  return (
+    <div>
+      <h1 className="header">Pokemon Go Card Name</h1>
+      <Pokegame />
+    </div>
+  );
+}
+
+export default App;
